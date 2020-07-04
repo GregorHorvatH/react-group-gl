b5c900a01238956c593b664b3d3dfe7565226e64
Geri <gregor.horvath83@gmail.com>
2020-07-04 18:09:03 +0300

    update

--- src/components/Todos/TodoItem/index.js
+++ src/components/Todos/TodoItem/index.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './styles.scss';
 
-const TodoItem = ({ todo, onDelete }) => {
-  return (
-    <li className="todo-item">
-      <span>{todo.text}</span>
-      <button onClick={onDelete}>x</button>
-    </li>
-  );
-};
+class TodoItem extends Component {
+  render() {
+    const { todo, onDelete } = this.props;
+
+    return (
+      <li className="todo-item">
+        <span>{todo.text}</span>
+        <button onClick={onDelete}>x</button>
+      </li>
+    );
+  }
+}
 
 export default TodoItem;
--- src/components/Todos/TodoList/index.js
+++ src/components/Todos/TodoList/index.js
@@ -1,18 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
+import FlipMove from 'react-flip-move';
 import TodoItem from '../TodoItem';
 import './styles.scss';
 
 const TodoList = ({ todos, onDelete }) => {
+  const [sortDirection, setSortDirection] = useState('desc');
+  const sortedTodos = [...todos].sort((a, b) =>
+    sortDirection === 'asc' ? a.id - b.id : b.id - a.id,
+  );
+
   return (
-    <ul className="todo-list">
-      {todos.map((todo) => (
-        <TodoItem
-          key={todo.id}
-          todo={todo}
-          onDelete={() => onDelete(todo.id)}
-        />
-      ))}
-    </ul>
+    <div className="list-wrapper">
+      <div className="filters">
+        <label>
+          <span>asc</span>
+          <input
+            className="button"
+            type="radio"
+            value="asc"
+            checked={sortDirection === 'asc'}
+            onChange={() => setSortDirection('asc')}
+          />
+        </label>
+        <label>
+          <span>desc</span>
+          <input
+            className="button"
+            type="radio"
+            value="desc"
+            checked={sortDirection === 'desc'}
+            onChange={() => setSortDirection('desc')}
+          />
+        </label>
+      </div>
+
+      <ul className="todo-list">
+        <FlipMove>
+          {sortedTodos.map((todo) => (
+            <TodoItem
+              key={todo.id}
+              todo={todo}
+              onDelete={() => onDelete(todo.id)}
+            />
+          ))}
+        </FlipMove>
+      </ul>
+    </div>
   );
 };
 
